a488347f1991389728cad670778136c507789f7d
Hiro <hiromichi.sase@gmail.com>
2025-01-20 11:27:20 +0900

    Previewで表示するデータ、ContentViewを変更

--- SwiftUICoreData/Persistence.swift
+++ SwiftUICoreData/Persistence.swift
@@ -14,9 +14,9 @@ struct PersistenceController {
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+        for index in 0..<10 {
+            let newMemo = Memo(context: viewContext)
+            newMemo.title = "title \(index + 1)"
         }
         do {
             try viewContext.save()
