cd7e99ed2d9985a22bf372b5d3ee0df8536321c1
Hiro <hiromichi.sase@gmail.com>
2025-01-22 16:20:10 +0900

    編集画面を実装

---
+++ SwiftUICoreData/EditMemoView.swift
@@ -0,0 +1,60 @@
+//
+//  EditMemoView.swift
+//  SwiftUICoreData
+//
+//  Created by Hiromichi Sase on 2025/01/22.
+//
+
+import SwiftUI
+
+struct EditMemoView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.presentationMode) var presentation
+    @State private var title: String = ""
+    @State private var content: String = ""
+    @State private var showDialog: Bool = false
+    private var memo: Memo
+
+    init(memo: Memo) {
+        self.memo = memo
+        self.title = title
+        self.content = content
+    }
+
+    var body: some View {
+        VStack {
+            TextField("Title", text: $title)
+                .padding(EdgeInsets(top: .zero, leading: 10.0, bottom: .zero, trailing: 10.0))
+                .border(Color.gray)
+
+            TextField("Content", text: $content)
+                .padding(EdgeInsets(top: .zero, leading: 10.0, bottom: .zero, trailing: 10.0))
+                .border(Color.gray)
+
+            Spacer()
+
+            Button("Save") {
+                if title.isEmpty {
+                    showDialog = true
+                    return
+                }
+
+                memo.title = title
+                memo.content = content
+
+                try? viewContext.save()
+                presentation.wrappedValue.dismiss()
+            }
+            .buttonStyle(.bordered)
+            .tint(.green)
+            .alert(isPresented: $showDialog) {
+                Alert(title: Text("Title is empty"), message: Text("Please input title."))
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    EditMemoView(memo: Memo())
+}
