f9bad68a5f3fca90db1d8f91100c95eb63ca1d51
Hiro <hiromichi.sase@gmail.com>
2026-05-17 10:43:26 +0900

    Memoに項目追加(id, createdAt, updatedAt, order)

--- SwiftUICoreData/Memo+CoreDataProperties.swift
+++ SwiftUICoreData/Memo+CoreDataProperties.swift
@@ -16,8 +16,12 @@ extension Memo {
         return NSFetchRequest<Memo>(entityName: "Memo")
     }
 
+    @NSManaged public var id: UUID
     @NSManaged public var title: String
     @NSManaged public var content: String
+    @NSManaged public var createdAt: Date
+    @NSManaged public var updatedAt: Date
+    @NSManaged public var order: Int
 
 }
 
